4a973fc6e7b138d369fc4c4ce433b9b9d95b02f3
FatulM <madani_amirreza@yahoo.com>
2020-10-30 19:32:50 +0330

    REFACTOR app class

--- lib/app/cryptolist_app.dart
+++ lib/app/cryptolist_app.dart
@@ -4,36 +4,32 @@ import 'package:flutter/material.dart';
 class CryptolistApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final base = ThemeData.light();
-
-    final theme = base.copyWith(
-      visualDensity: VisualDensity.adaptivePlatformDensity,
-      primaryColor: Colors.blue,
-      accentColor: Colors.tealAccent,
-      backgroundColor: Colors.white,
-      scaffoldBackgroundColor: Colors.white,
-      appBarTheme: AppBarTheme(
-        elevation: 0.0,
-        shadowColor: Colors.transparent,
-        color: Colors.white,
-        actionsIconTheme: IconThemeData(
-          color: Colors.black,
-        ),
-        iconTheme: IconThemeData(
-          color: Colors.black,
-        ),
-        textTheme: TextTheme(
-          headline6: TextStyle(
+    return MaterialApp(
+      title: 'CryptoList',
+      theme: ThemeData.light().copyWith(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Colors.blue,
+        accentColor: Colors.tealAccent,
+        backgroundColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          elevation: 0.0,
+          shadowColor: Colors.transparent,
+          color: Colors.white,
+          actionsIconTheme: IconThemeData(
             color: Colors.black,
-            fontSize: 32.0,
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          textTheme: TextTheme(
+            headline6: TextStyle(
+              color: Colors.black,
+              fontSize: 32.0,
+            ),
           ),
         ),
       ),
-    );
-
-    return MaterialApp(
-      title: 'CryptoList',
-      theme: theme,
       home: ListPage(),
     );
   }
